69d13a1ce9bdec0a914ea885e0aec930c3a344d2
doodlum <15017472+doodlum@users.noreply.github.com>
2026-08-22 21:34:20 +0100

    Fix SKSE AE structure compatibility metadata

--- src/main.cpp
+++ src/main.cpp
@@ -7,7 +7,7 @@ DLLEXPORT constinit auto SKSEPlugin_Version = []() noexcept {
 	data.PluginName(Plugin::NAME);
 	data.AuthorName(Plugin::AUTHOR);
 	data.UsesAddressLibrary();
-	data.UsesNoStructs();
+	data.UsesUpdatedStructs();
 
 	return data;
 }();
